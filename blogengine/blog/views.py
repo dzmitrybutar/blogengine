647189ffe740f399cbd4d2a667f17b31c613dbb4
dzmitrybutar <verysweetleaf@gmail.com>
2019-04-10 16:23:59 +0300

    Create base templates

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def posts_list(request):
-    return HttpResponse('<h1>hello<h1>')
+    n = ['pidr', 'loh']
+    return render(request, 'blog/index.html', context={'names': n})
 
