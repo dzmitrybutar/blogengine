f2baec4df69b3e70f004af77740697e4b381ac2b
dzmitrybutar <verysweetleaf@gmail.com>
2019-04-12 13:52:22 +0300

    Create models, templates

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def posts_list(request):
-    n = ['pidr', 'loh']
-    return render(request, 'blog/index.html', context={'names': n})
+from .models import Post
 
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post':post})
