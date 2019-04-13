9e7c3e957e6b3174dd07a12df4057cebce2aaf1e
dzmitrybutar <verysweetleaf@gmail.com>
2019-04-13 15:37:45 +0300

    Add form for tags

---
+++ blogengine/blog/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Tag
+from django.core.exceptions import ValidationError
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['title', 'slug']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "Create"')
+        if Tag.objects.filter(slug__iexact=new_slug).count():
+            raise ValidationError('slug estb')
+        return new_slug
+
+   # def save(self):
+        #new_tag = Tag.objects.create(
+           # title=self.cleaned_data['title'],
+            #slug=self.cleaned_data['slug'])
+       # return new_tag
