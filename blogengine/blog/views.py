3347c8e85bf8d28c4fae1b9f955ad84be6a27ef5
dzmitrybutar <verysweetleaf@gmail.com>
2019-04-15 14:31:07 +0300

    Add search by title and text

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -9,9 +9,14 @@ from .forms import TagForm, PostForm
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
-
+from django.db.models import Q
 def posts_list(request):
-    posts = Post.objects.all()
+    search_q = request.GET.get('search', '')
+    if search_q:
+        posts = Post.objects.filter(Q(title__icontains=search_q) | Q(body__icontains=search_q))
+    else:
+        posts = Post.objects.all()
+
     paginator = Paginator(posts, 2)
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
